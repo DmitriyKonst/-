9ec4fc7e4a40c128abc2a2162c26e98e5e3d04fe
DmitriyKonst <konstantinovd581@gmail.com>
2024-09-28 21:10:19 +0400

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,5 @@
+homework = 12
+hour = 1.5
+name_class = "Python"
+time_1_lesson = hour / homework
+print('Курс:',name_class,'Всего задач:',homework, 'Затрачено часов:',hour,'Среднее время выполнения',time_1_lesson,'часа.')
